8fc44f4dc41469e9d4894ae1631784fa50935934
tehilafavourite <tehilafavourite@gmail.com>
2022-06-25 13:38:18 +0100

    first commit

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,32 +4,60 @@ pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "./RewardsToken.sol";
-import "./NFT.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/ownable.sol";
+import "./events.sol";
 
-contract Staking is ERC721Holder {
-    NFT public nft;
-    RewardsToken public rewardsToken;
-    address public owner;
+contract Staking is ERC721Holder, events {
+    IERC721 public StakeNFT;
+    IERC20 public rewardsToken;
     uint256 public totalStaked;
 
     struct Stake {
-        address userAddr;
-        uint256 tokenID;
-        uint256 amount;
+        uint256[] tokenID;
+        uint256 totalRewards;
         uint256 time;
-    }
-
-    constructor(NFT _nft, RewardsToken _token) {
-        nft = _nft;
-        rewardsToken = _token;
-        owner = msg.sender;
     }
 
     mapping(address => Stake) public stakes;
 
+    constructor(address _nft, address _token) {
+        StakeNFT = IERC721(_nft);
+        rewardsToken = IERC20(_token);
+    }
+
     function stake(uint256[] memory _tokenIds) external {
-        uint256 tokenIds;
-        totalStaked += _tokenIds.length;
+        // get the length of tokenID
+        uint256 lent = _tokenIds.length;
+        // declare user storage file
+        Stake storage userStake = stakes[_msgSender()];
+        // bool to check if the user own any token it will be true
+        // otherwise no state variable will be initialized.
+        bool ownAny;
+        for (uint256 i; i < lent; ) {
+            // safe to get the owner of the token to be staked
+            address isOwner = StakeNFT.ownerOf(_tokenIds[i]);
+            // if caller is the owner of the ID, then update it state
+            if (isOwner == _msgSender()) {
+                userStake.tokenID.push(_tokenIds[i]); 
+                totalStaked ++;
+                if (!ownAny) {
+                    // update this state once for gas efficiency
+                    ownAny = true;
+                }
+                // emmit stake event
+                emit Stake(_msgSender(), _tokenIds[i], block.timestamp);
+            }           
+            unchecked {
+                i++;
+            }
+        }
+        // if caller has any token ID from the inputs IDs then `ownAny` will be initialized to true
+        if(ownAny) {
+            // initialize user time
+            userStake.time = block.timestamp;
+            // updating user rewards to add 0
+            userStake.totalRewards += 0;
+        }
     }
 }
---
+++ contracts/events.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+/**
+* @dev Emitted when `stake` tokens are moved from user to contract  (`user`) to
+* another (`address(this)`), and the `tokenID` then the `time` user staked.
+*
+* Note that `value` may be zero.
+*/
+event Staked(address indexed user, uint256 tokenID, uint256 timeStake)
